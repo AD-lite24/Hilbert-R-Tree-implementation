843f93be75f5dbb3ef1b446cdfef1a10ff51e9b3
Aditya Dandwate <adityadandwate24@gmail.com>
2023-04-24 10:31:02 +0530

    refined code

--- RTreeChatGPT.c
+++ RTreeChatGPT.c
@@ -247,3 +247,75 @@ void print_hilbert_value(void *data) {
     HRTreeData *d = (HRTreeData *)data;
     printf("Hilbert value: %d\n", d->hilbert_value);
 }
+
+// node represents a tree node of the tree.
+// C code based on explanation of ChatGPT
+// struct HRtree {
+//     int min, max, bits;
+//     struct node *root;
+//     struct h *hf;
+//     int size;
+// };
+// struct HRtree *new_tree(int min, int max, int bits) {
+//     struct HRtree *rt = malloc(sizeof(struct HRtree));
+//     if (rt == NULL) {
+//         perror("malloc error");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     rt->hf = NULL;
+//     rt->root = NULL;
+
+//     if (bits < DEFAULT_RESOLUTION) {
+//         bits = DEFAULT_RESOLUTION;
+//     }
+
+//     if (min < 0) {
+//         min = DEFAULT_MIN_NODE_ENTRIES;
+//     }
+
+//     if (max < 0) {
+//         max = DEFAULT_MAX_NODE_ENTRIES;
+//     }
+
+//     if (max < min) {
+//         fprintf(stderr, "Minimum number of nodes should be less than Maximum number of nodes and not vice versa.\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     struct h *hf = NULL;
+//     int err = h_new(&hf, (uint32_t) bits, DIM);
+
+//     if (err != 0) {
+//         fprintf(stderr, "Error creating Hilbert curve instance\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     struct node *root = new_node(min, max);
+//     root->leaf = 1;
+
+//     rt->min = min;
+//     rt->max = max;
+//     rt->bits = bits;
+//     rt->hf = hf;
+//     rt->root = root;
+
+//     return rt;
+// }
+// type node struct
+// {
+//     min, max int
+//     parent *node
+//     left, right *node
+//     leaf bool
+//     entries *entryList
+//      lhv *big.Int
+//      bb *rectangle // bounding-box of all children of this entry
+// }
+// type entry struct {
+// 	bb   *rectangle // bounding-box of of this entry
+// 	node *node
+// 	obj  Rectangle
+// 	h    *big.Int // hilbert value
+// 	leaf bool
+// }
